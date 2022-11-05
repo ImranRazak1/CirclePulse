4ccc26cb67c06da663c5386d728dbd5760d48cd6
Imran Razak <Imranrazak1@icloud.com>
2022-11-05 19:08:03 +0000

    Creating Circle Pulsing.

---
+++ CirclePulse/PulsingView.swift
@@ -0,0 +1,74 @@
+//
+//  PulsingView.swift
+//  CirclePulse
+//
+//  Created by Imran razak on 05/11/2022.
+//
+
+import Foundation
+import UIKit
+
+class PulsingView: UIView {
+    init() {
+        super.init(frame: .zero)
+        layer.addSublayer(pulsingLayer)
+        layer.addSublayer(mainLayer)
+        pulsingLayer.add(animationGroup, forKey: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var animationGroup: CAAnimationGroup = {
+        let animationGroup = CAAnimationGroup()
+        animationGroup.animations = [expandingAnimation,
+                                     fadingAnimation]
+        animationGroup.duration = 5.0
+        animationGroup.repeatCount = .infinity
+        animationGroup.autoreverses = true
+        return animationGroup
+    }()
+    
+    private lazy var fadingAnimation: CABasicAnimation = {
+        let fadingAnimation = CABasicAnimation(keyPath: "opacity")
+        fadingAnimation.fromValue = 1
+        fadingAnimation.toValue = 0
+        return fadingAnimation
+    }()
+    
+    private lazy var expandingAnimation: CABasicAnimation = {
+        let expandingAnimation = CABasicAnimation(keyPath: "transform.scale")
+        expandingAnimation.fromValue = 1
+        expandingAnimation.toValue = 1.4
+        return expandingAnimation
+    }()
+    
+    private let mainLayer: CAShapeLayer = circleLayer(color: Constant.mainColor)
+    private let pulsingLayer: CAShapeLayer = circleLayer(color: Constant.pulsingColor)
+    
+    private static func circleLayer(color: CGColor) -> CAShapeLayer {
+        let circleLayer = CAShapeLayer()
+        circleLayer.path = Constant.bezierPath.cgPath
+        circleLayer.lineWidth = 50
+        circleLayer.strokeColor = color
+        circleLayer.fillColor = UIColor.clear.cgColor
+        return circleLayer
+    }
+    
+    private enum Constant {
+        static let bezierPath: UIBezierPath = .init(arcCenter: CGPoint.zero,
+                                                    radius: 100,
+                                                    startAngle: -(CGFloat.pi / 2),
+                                                    endAngle: -(CGFloat.pi / 2) + (2 * CGFloat.pi),
+                                                    clockwise: true)
+        static let mainColor: CGColor = UIColor(red: 0.12,
+                                                green: 0.56,
+                                                blue: 1,
+                                                alpha: 1).cgColor
+        static let pulsingColor: CGColor = UIColor(red: 0,
+                                                   green: 0.75,
+                                                   blue: 1,
+                                                   alpha: 1).cgColor
+    }
+}
